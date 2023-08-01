faac7b0da81cdfb56b79b54a44302b72efc5d280
Ryuji Eguchi <jp.ryuji@gmail.com>
2023-08-02 06:18:32 +1000

    add sample test

---
+++ spec/models/sample_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Sample' do
+  describe 'sample' do
+    it 'returns true' do
+      expect(true).to be_truthy
+    end
+  end
+end
